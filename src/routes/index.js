486db8cf0510cc170a3e2f4686b7ea9628172c1c
LuanAlvesScar <135280874+LuanAlvesScar@users.noreply.github.com>
2023-06-01 19:55:02 -0300

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,42 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Welcome from "../pages/Welcome";
+import SignIn from "../pages/SignIn";
+import Home from "../pages/Home";
+import TelaInformacoes from "../pages/TelaInformacoes";
+import Perfil from "../pages/Perfil";
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="TelaInformacoes"
+        component={TelaInformacoes}
+        options={{ headerShown: false }}
+      />
+       
+      <Stack.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
